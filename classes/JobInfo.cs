849dc1faf702c4c760e3b99657dc3677a891655c
Chris Lovett <chris.lovett02@yahoo.net>
2023-04-22 01:37:28 -0400

    created JobInfo class.

---
+++ classes/JobInfo.cs
@@ -0,0 +1,21 @@
+namespace JobHunt
+{
+    public class JobInfo
+    {
+        public Guid Id { get; set; }
+        public string? CompanyName { get; set; }
+        public string? JobTitle { get; set; }
+        public string? JobDescription { get; set; }
+        public string? State { get; set; }
+        public string? City { get; set;}
+        public bool Remote { get; set; }
+        public bool Hybrid { get; set; }
+        public bool Onsite { get; set; }
+        // public string? SkillRequirements { get; set; }
+        public DateOnly ApplicationDate { get; set; }
+        public DateTime ApplicationTime { get; set; }
+        public bool Responded { get; set; }
+        public DateTime ResponseDate { get; set; }
+
+    }
+}
