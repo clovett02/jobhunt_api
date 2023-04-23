83864727088c241e8bd237d9974b87dcb174404d
Chris Lovett <chris.lovett02@yahoo.net>
2023-04-22 21:31:29 -0400

    refactored post request. Place insert sql command within a function

--- Controllers/JobInfoController.cs
+++ Controllers/JobInfoController.cs
@@ -11,10 +11,42 @@ public class JobInfoController : ControllerBase
     {
         return "Hello";
     }
-    
+
     [HttpPost("/jobinfo")]
     public void Post([FromBody] JobRecord Job)
     {
+        void InsertJobs(MySqlConnection con)
+        {
+            var sql = @"INSERT INTO jobs(CompanyName, JobTitle, JobDescription, State, City, Remote, Hybrid, Onsite, 
+            ApplicationDate, ApplicationTime, Responded, ResponseDate, ResponseTime) 
+            
+            VALUES(@CompanyName, @JobTitle, @JobDescription, @State, @City, @Remote, @Hybrid, @Onsite, 
+            @ApplicationDate, @ApplicationTime, @Responded, @ResponseDate, @ResponseTime)";
+
+            using var cmd = new MySqlCommand(sql, con);
+
+            cmd.Parameters.AddWithValue("@CompanyName", Job.CompanyName);
+            cmd.Parameters.AddWithValue("@JobTitle", Job.JobTitle);
+            cmd.Parameters.AddWithValue("@JobDescription", Job.JobDescription);
+            cmd.Parameters.AddWithValue("@State", Job.State);
+            cmd.Parameters.AddWithValue("@City", Job.City);
+            cmd.Parameters.AddWithValue("@Remote", Job.Remote);
+            cmd.Parameters.AddWithValue("@Hybrid", Job.Hybrid);
+            cmd.Parameters.AddWithValue("@Onsite", Job.Onsite);
+            cmd.Parameters.AddWithValue("@ApplicationDate", Job.ApplicationDate);
+            cmd.Parameters.AddWithValue("@ApplicationTime", Job.ApplicationTime);
+            cmd.Parameters.AddWithValue("@Responded", Job.Responded);
+            cmd.Parameters.AddWithValue("@ResponseDate", Job.ResponseDate);
+            cmd.Parameters.AddWithValue("@ResponseTime", Job.ResponseTime);
+
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+        }
+        void InsertSkills(MySqlConnection con)
+        {
+            
+        }
+
         string cs = @"
             server=hulk.mysql;
             userid=root;
@@ -25,33 +57,14 @@ public class JobInfoController : ControllerBase
         using var con = new MySqlConnection(cs);
         con.Open();
 
-        var sql = @"INSERT INTO jobs(CompanyName, JobTitle, JobDescription, State, City, Remote, Hybrid, Onsite, 
-            ApplicationDate, ApplicationTime, Responded, ResponseDate, ResponseTime) 
-            
-            VALUES(@CompanyName, @JobTitle, @JobDescription, @State, @City, @Remote, @Hybrid, @Onsite, 
-            @ApplicationDate, @ApplicationTime, @Responded, @ResponseDate, @ResponseTime)";
+        InsertJobs(con);
+        InsertSkills(con);
 
-        using var cmd = new MySqlCommand(sql, con);
-
-        cmd.Parameters.AddWithValue("@CompanyName", $"{Job.CompanyName}");
-        cmd.Parameters.AddWithValue("@JobTitle", $"{Job.JobTitle}");
-        cmd.Parameters.AddWithValue("@JobDescription", $"{Job.JobDescription}");
-        cmd.Parameters.AddWithValue("@State", $"{Job.State}");
-        cmd.Parameters.AddWithValue("@City", $"{Job.City}");
-        cmd.Parameters.AddWithValue("@Remote", $"{Job.Remote}");
-        cmd.Parameters.AddWithValue("@Hybrid", $"{Job.Hybrid}");
-        cmd.Parameters.AddWithValue("@Onsite", $"{Job.Onsite}");
+        
 
         //Skills required attribute will be looped thru and added seperately to the skills table
 
-        cmd.Parameters.AddWithValue("@ApplicationDate", $"{Job.ApplicationDate}");
-        cmd.Parameters.AddWithValue("@", $"{Job.ApplicationTime}");
-        cmd.Parameters.AddWithValue("@", $"{Job.Responded}");
-        cmd.Parameters.AddWithValue("@", $"{Job.ResponseDate}");
-        cmd.Parameters.AddWithValue("@", $"{Job.ResponseTime}");
-
-        cmd.Prepare();
-        cmd.ExecuteNonQuery();   
+           
 
     }
 }
