423ad502c0171778b05a9d19b049cf0077e7f494
Chris Lovett <chris.lovett02@yahoo.net>
2023-05-03 02:23:49 -0400

    added cors

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+{
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
