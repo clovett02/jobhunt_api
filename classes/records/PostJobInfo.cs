69a754f86408e212e1bd1da0cde608661d23715e
Chris Lovett <chris.lovett02@outlook.com>
2025-01-05 23:05:53 -0500

    created seperate record classes and files for "get" and "post" requests

---
+++ classes/records/PostJobInfo.cs
@@ -0,0 +1,16 @@
+namespace JobHunt_API
+{
+    public record PostJobInfo
+    (
+        string CompanyName,
+        string JobTitle,
+        string JobDescription,
+        string State, 
+        string City,
+        bool Remote,
+        bool Hybrid,
+        bool Onsite,
+        DateTime ApplicationDate,
+        DateTime ApplicationTime
+    );
+}
