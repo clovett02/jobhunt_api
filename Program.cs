e1af1d682c6d6e116be36d6e26ed4ed2962447cf
Chris Lovett <chris.lovett02@outlook.com>
2025-01-10 06:41:21 -0500

    enable endpoint api explorer and swaggergen

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options => 
 {
